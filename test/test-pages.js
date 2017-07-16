f95894fdbd0d5c35f0a66a8542c3bb1f4a272bd3
Lachlan McKerrow <lachlan@mckerrow.com>
2017-07-16 11:23:16 +1000

    Added describe

--- test/test-pages.js
+++ test/test-pages.js
@@ -2,19 +2,27 @@
 var expect = require('chai').expect;
 var request = require('request');
 
-it('Main page content', function () {
-    request('http://localhost:8080', function (error, response, body) {
-        expect(body).to.equal('Hello World');
-    });
-});
-it('Main page status', function () {
-    request('http://localhost:8080', function (error, response, body) {
-        expect(response.statusCode).to.equal(200);
-    });
-});
+describe('Status and content', function () {
+    describe('Main page', function () {
+        it('status', function () {
+            request('http://localhost:8080/', function (error, response, body) {
+                expect(response.statusCode).to.equal(200);
+            });
+        });
 
-it('About page content', function () {
-    request('http://localhost:8080/about', function (error, response, body) {
-        expect(response.statusCode).to.equal(404);
+        it('content', function () {
+            request('http://localhost:8080/', function (error, response, body) {
+                expect(body).to.equal('Hello World');
+            });
+        });
+    });
+
+    describe('About page', function () {
+        it('status', function () {
+            request('http://localhost:8080/about', function (error, response, body) {
+                expect(response.statusCode).to.equal(404);
+            });
+        });
+
     });
 });
